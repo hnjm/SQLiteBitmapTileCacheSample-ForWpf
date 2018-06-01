35ec5fffbcc2fd84bf6a35802a8bc7bb9641d78e
tangyijun@thinkgeo.com <tangyijun@thinkgeo.com>
2018-06-01 13:04:06 +0800

    Initial commit

---
+++ SQLiteBitmapTileCacheSample/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Wpf;
+
+namespace SQLiteBitmapTileCacheSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void map_Loaded(object sender, RoutedEventArgs e)
+        {
+            map.MapUnit = GeographyUnit.DecimalDegree;
+
+            WorldStreetsAndImageryOverlay layerOverlay = new WorldStreetsAndImageryOverlay();
+            layerOverlay.TileCache = new SQLiteBitmapTileCache();
+            map.Overlays.Add(layerOverlay);
+
+            map.CurrentExtent = new RectangleShape(-180, 180, 180, -180);
+            map.Refresh();
+        }
+    }
+}
