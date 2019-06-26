a45bc45048f0e074cf8a8cf4d711320ea86665a7
RexLiu <rexliu@thinkgeo.com>
2019-06-26 14:11:59 +0800

    Update packages version to MapSuite10.5.

--- SQLiteBitmapTileCacheSample/MainWindow.xaml.cs
+++ SQLiteBitmapTileCacheSample/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿/*===========================================
+    Backgrounds for this sample are powered by ThinkGeo Cloud Maps and require
+    a Client ID and Secret. These were sent to you via email when you signed up
+    with ThinkGeo, or you can register now at https://cloud.thinkgeo.com.
+===========================================*/
+
+using System.Windows;
 using ThinkGeo.MapSuite;
 using ThinkGeo.MapSuite.Layers;
 using ThinkGeo.MapSuite.Shapes;
@@ -21,12 +27,8 @@ namespace SQLiteBitmapTileCacheSample
             map.MapUnit = GeographyUnit.Meter;
             map.ZoomLevelSet = new ThinkGeoCloudMapsZoomLevelSet();
 
-            /*===========================================
-               Backgrounds for this sample are powered by ThinkGeo Cloud Maps and require
-               a Client ID and Secret. These were sent to you via email when you signed up
-               with ThinkGeo, or you can register now at https://cloud.thinkgeo.com.
-            ===========================================*/
-            ThinkGeoCloudRasterMapsOverlay baseOverlay = new ThinkGeoCloudRasterMapsOverlay();
+            // Please input your ThinkGeo Cloud Client ID / Client Secret to enable the background map. 
+            ThinkGeoCloudRasterMapsOverlay baseOverlay = new ThinkGeoCloudRasterMapsOverlay("ThinkGeo Cloud Client ID", "ThinkGeo Cloud Client Secret");
             baseOverlay.TileCache = new SQLiteBitmapTileCache();
             map.Overlays.Add(baseOverlay);
 
